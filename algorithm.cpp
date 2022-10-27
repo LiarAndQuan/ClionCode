ba6c27bed7f5e3fc0d8f352b28ea93287ca3328b
LiarAndQuan <3218684870@qq.com>
2022-10-27 22:55:50 +0800

    多重背包的二进制优化

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,22 +5,34 @@
 #define endl '\n'
 using namespace std;
 
-int v[200], w[200], f[200];
+int vv[100], ww[100], f[100];
 
 signed main() {
-
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int num = 1;
     int n, m;
     cin >> n >> m;
+    int v, w, s;
+
     for (int i = 1; i <= n; i++) {
-        cin >> v[i] >> w[i];
-    }
-    for (int i = 1; i <= n; i++) {
-        for (int j = w[i]; j <= m; j++) {
-            f[j] = max(f[j], f[j - w[i]] + v[i]);
+        cin >> v >> w >> s;
+        for (int j = 1; j <= s; j <<= 1) {
+            vv[num] = j * v;
+            ww[num++] = j * w;
+            s -= j;
+        }
+        if (s) {
+            vv[num] = s * v;
+            ww[num++] = s * w;
         }
     }
-    cout << f[m] << endl;
+
+    for (int i = 1; i < num; i++) {
+        for (int j = m; j >= vv[i]; j--) {
+            f[j] = max(f[j], f[j - vv[i]] + ww[i]);
+        }
+    }
+    cout << f[m];
 }
