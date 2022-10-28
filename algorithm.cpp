dc7023d24a80f16e3d42711e4ef6224650ce54d6
LiarAndQuan <3218684870@qq.com>
2022-10-28 11:41:42 +0800

    混合背包

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,7 +5,7 @@
 #define endl '\n'
 using namespace std;
 
-int f[200], g[200], q[200];
+int a[100], b[100], c[100], f[100];
 
 signed main() {
     ios::sync_with_stdio(false);
@@ -13,27 +13,45 @@ signed main() {
 
     int n, m;
     cin >> n >> m;
+    int num = 1;
     int v, w, s;
-
     for (int i = 1; i <= n; i++) {
         cin >> v >> w >> s;
-        memcpy(g, f, sizeof(f));
-        //准备一个备份数组,因为多重背包需要我们从后往前更新,但是单调队列需要我们从前往后更新
-        for (int j = 0; j < v; j++) {
-            int h = 0, t = -1;
-            for (int k = j; k <= m; k += v) {
-                if (h <= t && k - q[h] > s * v) {
-                    h++;
+        if (s == 0) {
+            a[num] = v;
+            b[num] = w;
+            c[num++] = 0;
+        } else {
+            if (s == -1) {
+                s = 1;
+                int k = 1;
+                while (s >= k) {
+                    a[num] = k * v;
+                    b[num] = k * w;
+                    c[num++] = 1;
+                    s -= k;
+                    k <<= 1;
                 }
-                if (h <= t) {
-                    f[k] = max(g[k], g[q[h]] + (k - q[h]) / v * w);
-                }
-                while (h <= t && g[k] >= g[q[t]] + (k - q[t]) / v * w) {
-                    t--;
-                }
-                q[++t] = k;
+            }
+            if (s) {
+                a[num] = s * v;
+                b[num] = s * w;
+                c[num++] = 1;
             }
         }
     }
-    cout << f[m];
+    for (int i = 1; i < num; i++) {
+        if (c[i] == 1) {
+            for (int j = m; j >= a[i]; j--) {
+                f[j] = max(f[j], f[j - a[i]] + b[i]);
+            }
+        } else {
+            for (int j = a[i]; j <= m; j++) {
+                f[j] = max(f[j], f[j - a[i]] + b[i]);
+            }
+        }
+    }
+    cout << f[m] << endl;
+
+
 }
