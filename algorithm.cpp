31b62126b9e49d8062aea24c11b2a54197335f90
LiarAndQuan <3218684870@qq.com>
2022-10-28 11:41:03 +0800

    多重背包的单调队列优化

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,33 +5,34 @@
 #define endl '\n'
 using namespace std;
 
-int vv[100], ww[100], f[100];
+int f[200], g[200], q[200];
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int num = 1;
     int n, m;
     cin >> n >> m;
     int v, w, s;
 
     for (int i = 1; i <= n; i++) {
         cin >> v >> w >> s;
-        for (int j = 1; j <= s; j <<= 1) {
-            vv[num] = j * v;
-            ww[num++] = j * w;
-            s -= j;
-        }
-        if (s) {
-            vv[num] = s * v;
-            ww[num++] = s * w;
-        }
-    }
-
-    for (int i = 1; i < num; i++) {
-        for (int j = m; j >= vv[i]; j--) {
-            f[j] = max(f[j], f[j - vv[i]] + ww[i]);
+        memcpy(g, f, sizeof(f));
+        //准备一个备份数组,因为多重背包需要我们从后往前更新,但是单调队列需要我们从前往后更新
+        for (int j = 0; j < v; j++) {
+            int h = 0, t = -1;
+            for (int k = j; k <= m; k += v) {
+                if (h <= t && k - q[h] > s * v) {
+                    h++;
+                }
+                if (h <= t) {
+                    f[k] = max(g[k], g[q[h]] + (k - q[h]) / v * w);
+                }
+                while (h <= t && g[k] >= g[q[t]] + (k - q[t]) / v * w) {
+                    t--;
+                }
+                q[++t] = k;
+            }
         }
     }
     cout << f[m];
