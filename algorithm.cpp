36ca1f32a43113833df1d71fe4313bd576a8d7e5
LiarAndQuan <3218684870@qq.com>
2022-10-28 11:42:07 +0800

    二维费用背包

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,53 +5,21 @@
 #define endl '\n'
 using namespace std;
 
-int a[100], b[100], c[100], f[100];
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, m;
-    cin >> n >> m;
-    int num = 1;
-    int v, w, s;
+    int n, V, M;
+    cin >> n >> V >> M;
+    int v, w, m, f[100][100];
     for (int i = 1; i <= n; i++) {
-        cin >> v >> w >> s;
-        if (s == 0) {
-            a[num] = v;
-            b[num] = w;
-            c[num++] = 0;
-        } else {
-            if (s == -1) {
-                s = 1;
-                int k = 1;
-                while (s >= k) {
-                    a[num] = k * v;
-                    b[num] = k * w;
-                    c[num++] = 1;
-                    s -= k;
-                    k <<= 1;
-                }
-            }
-            if (s) {
-                a[num] = s * v;
-                b[num] = s * w;
-                c[num++] = 1;
+        cin >> v >> m >> w;
+        for (int j = V; j >= v; j--) {
+            for (int k = M; k >= m; k--) {
+                f[j][k] = max(f[j][k], f[j - v][k - m] + w);
             }
         }
     }
-    for (int i = 1; i < num; i++) {
-        if (c[i] == 1) {
-            for (int j = m; j >= a[i]; j--) {
-                f[j] = max(f[j], f[j - a[i]] + b[i]);
-            }
-        } else {
-            for (int j = a[i]; j <= m; j++) {
-                f[j] = max(f[j], f[j - a[i]] + b[i]);
-            }
-        }
-    }
-    cout << f[m] << endl;
-
-
+    cout << f[V][M];
 }
