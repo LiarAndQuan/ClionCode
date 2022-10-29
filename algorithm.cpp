51117a3f28e1f4946064aad075fa48082d9f836d
LiarAndQuan <3218684870@qq.com>
2022-10-29 09:11:40 +0800

    背包问题求方案数

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,39 +5,30 @@
 #define endl '\n'
 using namespace std;
 
-int v[100], w[100], root, a[100][100], b[100], f[100][100];
-int n, m;
-
-void dfs(int u) {
-    for (int i = v[u]; i <= m; i++) {
-        f[u][i] = w[u];
-    }
-    for (int i = 0; i < b[u]; i++) {
-        int s = a[u][i];
-        dfs(s);
-        for (int j = m; j >= v[u]; j--) {//子父节点一共能用的容量
-            for (int k = 0; k <= j - v[u]; k++) {//将容量分配到子节点
-                f[u][j] = max(f[u][j], f[u][j - k] + f[s][k]);
-            }
-        }
-    }
-}
+int mod = 1e9 + 7;
 
 signed main() {
-
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n, m;
     cin >> n >> m;
+    int v, w, c[100], f[100];
+
+    for (int i = 0; i <= m; i++) {//不装物品也是一种方案
+        c[i] = 1;
+        f[i] = 0;
+    }
     for (int i = 1; i <= n; i++) {
-        int p;
-        cin >> v[i] >> w[i] >> p;
-        if (p == -1) {
-            root = i;
-        } else {
-            a[p][b[p]++] = i;
+        cin >> v >> w;
+        for (int j = m; j >= v; j--) {
+            if (f[j - v] + w > f[j]) {//只是多装入一件物品,没有改变方案数
+                f[j] = f[j - v] + w;
+                c[j] = c[j - v];
+            } else if (f[j - v] + w == f[j]) {
+                c[j] = (c[j] + c[j - v]) % mod;
+            }
         }
     }
-    dfs(root);
-    cout << f[root][m];
+    cout << c[m];
 }
