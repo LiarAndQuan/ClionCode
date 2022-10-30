5389ac5bb7ffbe25160e5f79edad110853eb5eac
LiarAndQuan <3218684870@qq.com>
2022-10-30 10:34:21 +0800

    背包问题求具体方案

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,7 +5,6 @@
 #define endl '\n'
 using namespace std;
 
-int mod = 1e9 + 7;
 
 signed main() {
     ios::sync_with_stdio(false);
@@ -13,22 +12,23 @@ signed main() {
 
     int n, m;
     cin >> n >> m;
-    int v, w, c[100], f[100];
-
-    for (int i = 0; i <= m; i++) {//不装物品也是一种方案
-        c[i] = 1;
-        f[i] = 0;
-    }
+    int v[100], w[100], f[100][100];
     for (int i = 1; i <= n; i++) {
-        cin >> v >> w;
-        for (int j = m; j >= v; j--) {
-            if (f[j - v] + w > f[j]) {//只是多装入一件物品,没有改变方案数
-                f[j] = f[j - v] + w;
-                c[j] = c[j - v];
-            } else if (f[j - v] + w == f[j]) {
-                c[j] = (c[j] + c[j - v]) % mod;
+        cin >> v[i] >> w[i];
+    }
+    for (int i = n; i >= 1; i--) {
+        for (int j = 0; j <= m; j++) {
+            f[i][j] = f[i + 1][j];
+            if (j >= v[i]) {
+                f[i][j] = max(f[i][j], f[i + 1][j - v[i]] + w[i]);
             }
         }
     }
-    cout << c[m];
+    int j = m;
+    for (int i = 1; i <= n; i++) {
+        if (j >= v[i] && f[i][j] == f[i + 1][j - v[i]] + w[i]) {
+            cout << i << " ";
+            j -= v[i];
+        }
+    }
 }
