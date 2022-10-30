12a6ba0ecbac7483bffdf12b6afa66bc65224d75
LiarAndQuan <3218684870@qq.com>
2022-10-30 10:37:03 +0800

    最长上升子序列(二分)

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,41 +5,44 @@
 #define endl '\n'
 using namespace std;
 
-int a[101];
-int f[101];
-int n;
-int path[101];
-int maxindex;
+int b[100005];
+int p[100005];
+int len = 0;
 
-void print(inti) {
-    if (path[i]) print(path[i]);
-    cout << a[i] << " ";
+int f(int x) {
+    int l = 1, r = len;
+    int mid, res;
+    while (l <= r) {
+        mid = (l + r) / 2;
+        if (p[mid] >= x) {
+            r = mid - 1;
+            res = mid;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return res;
 }
 
-signed main() {
+int main() {
 
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    int n;
     cin >> n;
-    int ans = 1;
 
     for (int i = 1; i <= n; i++) {
-        cin >> a[i];
-        f[i] = 1;
+        cin >> b[i];
     }
 
-    for (int i = 2; i <= n; i++) {
-        for (int j = 1; j < i; j++) {
-            if (a[i] > a[j] && f[j] + 1 > f[i]) {
-                f[i] = f[j] + 1;
-                path[i] = j;//记录前驱节点
-            }
-        }
-        if (f[i] > ans) {
-            ans = f[i];
-            maxindex = i;
+    for (int i = 1; i <= n; i++) {
+        if (b[i] > p[len]) {
+            p[++len] = b[i];
+        } else {
+            int index = f(b[i]);
+            p[index] = b[i];
         }
     }
-    cout << ans <<endl;
-    print(maxindex);
+    cout << len;
 }
