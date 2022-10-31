6fb33e4ad36131548d198ed5cd96ffcacc191117
LiarAndQuan <3218684870@qq.com>
2022-10-31 13:01:53 +0800

    滑动窗口最大值(单调队列)

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,54 +5,29 @@
 #define endl '\n'
 using namespace std;
 
-string a, b;
-int p[201][201];
-int f[201][201];
-char s[201];
 
-int main() {
-
+signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cin >> a >> b;
-    int m = a.length();
-    int n = b.length();
+    int n;
+    int nums[100];
+    int q[100];
+    int m;
+    cin >> n >> m;
+    int h = 0, t = -1;
 
-    //动态规划找到最长的并且标记路径
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (a[i - 1] == b[j - 1]) {//左上
-                f[i][j] = f[i - 1][j - 1] + 1;
-                p[i][j] = 1;
-            } else {//左
-                if (f[i][j - 1] > f[i - 1][j]) {
-                    f[i][j] = f[i][j - 1];
-                    p[i][j] = 2;
-                } else {//上
-                    f[i][j] = f[i - 1][j];
-                    p[i][j] = 3;
-                }
-            }
+    for (int i = 1; i <= n; i++) {
+        cin >> nums[i];
+        if (h <= t && i - q[h] > m - 1) {
+            h++;
+        }
+        while (h <= t && nums[i] >= nums[q[t]]) {
+            t--;
+        }
+        q[++t] = i;
+        if (i > m - 1) {
+            cout << nums[q[h]];
         }
     }
-
-    cout << f[m][n] << endl;
-
-    int i = m, j = n, k = f[m][n];
-
-    while (i > 0 && j > 0) {
-        if (p[i][j] == 1) {
-            s[k--] = a[i - 1];
-            i--, j--;
-        } else if (p[i][j] == 2) {
-            j--;
-        } else {
-            i--;
-        }
-    }
-    for (i = 1; i <= f[m][n]; i++) {
-        cout << s[i] << " ";
-    }
-
 }
