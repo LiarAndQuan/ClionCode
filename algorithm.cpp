a2aca919d54c3813d083f20b64515087c19385de
LiarAndQuan <3218684870@qq.com>
2022-11-01 13:55:50 +0800

    没有上司的舞会(洛谷P1352)(树形DP)

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,32 +5,37 @@
 #define endl '\n'
 using namespace std;
 
+int n;
+int w[100], fa[100], f[100][100];
+int a[100][100], b[100];
+
+void dfs(int u) {
+    f[u][1] = w[u];//选u
+    for (int i = 0; i < b[u]; i++) {
+        int son = a[u][i];
+        dfs(son);
+        f[u][0] += max(f[son][0], f[son][1]);
+        f[u][1] += f[son][0];
+    }
+}
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-
-    int n, m;
-    int res = -1;
-    cin >> n >> m;
-    int nums[100];
-    int q[100];
-
-    int h = 0, t = 0;
-    q[0] = 0;//提前给q[0]赋值,表示从第一个开始前m个最小值为0
-
+    cin >> n;
     for (int i = 1; i <= n; i++) {
-        cin >> nums[i];
-        if (h <= t && i - q[h] > m) {
-            h++;
-        }
-        res = max(res, nums[i] - nums[q[h]]);
-        while (h <= t && nums[i] <= nums[q[t]]) {
-            t--;
-        }
-        q[++t] = i;
+        cin >> w[i];
     }
 
-    cout << res;
+    for (int i = 0; i < n - 1; i++) {
+        int x, y;
+        cin >> x >> y;
+        a[y][b[y]++] = x;
+        fa[x] = true;//x有父节点
+    }
+    int root = 1;
+    while (fa[root]) root++;//找到根节点
+    dfs(root);
+    cout << max(f[root][0], f[root][1]);
 }
