7c05232c4b76a5a7de4ea5c82571dc505e547853
LiarAndQuan <3218684870@qq.com>
2022-11-01 13:58:11 +0800

    树的重心

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,37 +5,47 @@
 #define endl '\n'
 using namespace std;
 
-int n;
-int w[100], fa[100], f[100][100];
-int a[100][100], b[100];
 
-void dfs(int u) {
-    f[u][1] = w[u];//选u
-    for (int i = 0; i < b[u]; i++) {
-        int son = a[u][i];
-        dfs(son);
-        f[u][0] += max(f[son][0], f[son][1]);
-        f[u][1] += f[son][0];
+const int N = 1e5 + 10;
+int n, ans = N;
+bool vis[N];
+int h[N], e[2 * N], ne[2 * N], idx;
+
+void add(int a, int b) {
+    e[++idx] = b;
+    ne[idx] = h[a];
+    h[a] = idx;
+}
+
+int dfs(int u) {
+    vis[u] = true;
+    int size = 0;//最大子树的节点数
+    int sum = 1;//以u为根的子树的节点数
+    for (int i = h[u]; i != -1; i = ne[i]) {
+        int j = e[i];
+        if (vis[j]) {
+            continue;
+        }
+        int s = dfs(j);
+        size = max(size, s);
+        sum += s;
     }
+    ans = min(ans, max(size, n - sum));
+    return sum;
 }
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    memset(h, -1, sizeof(h));
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        cin >> w[i];
+    for (int i = 1; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        add(a, b);
+        add(b, a);
     }
-
-    for (int i = 0; i < n - 1; i++) {
-        int x, y;
-        cin >> x >> y;
-        a[y][b[y]++] = x;
-        fa[x] = true;//x有父节点
-    }
-    int root = 1;
-    while (fa[root]) root++;//找到根节点
-    dfs(root);
-    cout << max(f[root][0], f[root][1]);
+    dfs(1);
+    cout << ans;
 }
