bf4fad4fe75ecc62e51f36d6c91eb9d7d25a7cf3
LiarAndQuan <3218684870@qq.com>
2022-11-03 21:23:04 +0800

    大盗阿福(线性DP)

--- algorithm.cpp
+++ algorithm.cpp
@@ -6,51 +6,6 @@
 using namespace std;
 
 
-const int N = 100010, INF = 0x3f3f3f3ff;
-int n;
-int h[N], e[N * 2], w[N * 2], ne[N * 2], idx;
-int d[N];
-int f[N];
-int deg[N];
-
-void add(int a, int b, int c) {
-    e[++idx] = b;
-    w[idx] = c;
-    ne[idx] = h[a];
-    h[a] = idx;
-}
-
-int dfs_d(int u, int fa) {
-    for (inti = h[u]; i; i = ne[i]) {
-        int j = e[i];
-        if (j == fa) {
-            continue;
-        }
-        int s = dfs_d(j, u);
-        d[u] += min(w[i], s);
-    }
-    if (deg[u] == 1) {
-        return INF;
-    } else {
-        return d[u];
-    }
-}
-
-void dfs_f(int u, int fa) {
-    for (int i = h[u]; i; i = ne[i]) {
-        int j = e[i];
-        if (j == fa) {
-            continue;
-        }
-        if (deg[u] == 1) {
-            f[j] = d[j] + w[i];
-        } else {
-            f[j] = d[j] + min(w[i], f[u] - min(w[i], d[j]));
-        }
-        dfs_f(j, u);
-    }
-}
-
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -58,27 +13,18 @@ signed main() {
     int t;
     cin >> t;
     while (t--) {
+        int n;
         cin >> n;
-        idx = 0;
-        memset(h, 0, sizeof(h));
-        memset(d, 0, sizeof(d));
-        memset(f, 0, sizeof(f));
-        memset(deg, 0, sizeof(deg));
-        for (inti = 1; i <= n - 1; i++) {
-            int a, b, c;
-            cin >> a >> b >> c;
-            add(a, b, c);
-            add(b, a, c);
-            deg[a]++;
-            deg[b]++;
-        }
-        dfs_d(1, -1);
-        f[1] = d[1];//根只有向下的流量
-        dfs_f(1, -1);
-        int ans = 0;
+        int w[100];
+        int f[100];
         for (int i = 1; i <= n; i++) {
-            ans = max(ans, f[i]);
+            cin >> w[i];
         }
-        cout << ans << endl;
+        f[0] = 0;
+        f[1] = w[1];
+        for (int i = 2; i <= n; i++) {
+            f[i] = max(f[i - 1], f[i - 2] + w[i]);
+        }
+        cout<<f[n]<<endl;
     }
 }
