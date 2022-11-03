d4551c38495166ce6fd421805b66033500fcc3e0
LiarAndQuan <3218684870@qq.com>
2022-11-03 21:23:31 +0800

    股票买卖普通版

--- algorithm.cpp
+++ algorithm.cpp
@@ -10,21 +10,18 @@ signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        int w[100];
-        int f[100];
-        for (int i = 1; i <= n; i++) {
-            cin >> w[i];
-        }
-        f[0] = 0;
-        f[1] = w[1];
-        for (int i = 2; i <= n; i++) {
-            f[i] = max(f[i - 1], f[i - 2] + w[i]);
-        }
-        cout<<f[n]<<endl;
+    int n;
+    int w[100];
+    int f[100][2];
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> w[i];
     }
+    f[1][0] = 0;
+    f[1][1] = -w[1];
+    for (int i = 2; i <= n; i++) {
+        f[i][0] = max(f[i - 1][0], f[i - 1][1] + w[i]);
+        f[i][1] = max(f[i - 1][1], f[i - 1][0] - w[i]);
+    }
+    cout << f[n][0];
 }
