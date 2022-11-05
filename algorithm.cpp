5fe6ee82e498f3d7e2b6856f215194339dd4345e
LiarAndQuan <3218684870@qq.com>
2022-11-05 18:21:54 +0800

    链形石子合并

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,51 +5,30 @@
 #define endl '\n'
 using namespace std;
 
-bool st[100];
+
+int n;
+int a[100];
+int s[100];
+int f[100][100];
 
 signed main() {
-
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, m;
-    cin >> n >> m;
-    //预处理:判断合并列的状态i是否合法
-    //如果合并列的某行是1表示横放,是0表示竖放
-    //如果合并列不存在连续的奇数个0,即为合法状态,因为可以用竖着的方块补充
-    for (int i = 0; i < 1 << n; i++) {
-        st[i] = true;
-        int cnt = 0;//记录合并列中连续0的个数
-        for (int j = 0; j < n; j++) {
-            if (i >> j & 1) {//如果是1
-                if (cnt & 1) {//如果是奇数
-                    st[i] = false;
-                    break;
-                }
-            } else {
-                cnt++;
-            }
-        }
-        if (cnt & 1) {//处理前导0,例如001000中前两个0
-            st[i] = false;
-        }
+    memset(f, 0x3f, sizeof(f));
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        s[i] = s[i - 1] + a[i];
+        f[i][i] = 0;
     }
-
-    int f[100][100];
-
-    memset(f, 0, sizeof(f));
-
-    f[0][0] = 1;
-    for (int i = 1; i <= m; i++) {
-        for (int j = 0; j < 1 << n; j++) {//枚举第i列
-            for (int k = 0; k < 1 << n; k++) {//枚举第i-1列
-                if ((j & k) == 0 && st[j | k]) {
-                    //两列状态兼容:不出现重叠的1,不出现连续的奇数个0
-                    f[i][j] += f[i - 1][k];
-                }
+    for (int len = 2; len <= n; len++) {
+        for (int l = 1; l + len - 1 <= n; l++) {
+            int r = l + len - 1;
+            for (int k = l; k < r; k++) {
+                f[l][r] = min(f[l][r], f[l][k] + f[k + 1][r] + s[r] - s[l - 1]);
             }
         }
     }
-
-    cout << f[m][0] <<endl;
+    cout << f[1][n];
 }
