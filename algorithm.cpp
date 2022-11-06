50414598d2fa3d75972d2f2c2a692ecab0586397
LiarAndQuan <3218684870@qq.com>
2022-11-06 13:54:50 +0800

    能量项链

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,43 +1,33 @@
 #include<bits/stdc++.h>
+
 #define int long long
 #define double long double
 #define endl '\n'
 using namespace std;
 
+int n;
+int a[200], f[200][200];
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    int n;
-    int a[100], s[100], f[100][100], g[100][100];
-
-    memset(f, 0x3f, sizeof(f));
-    memset(g, -0x3f, sizeof(g));
 
     cin >> n;
     for (int i = 1; i <= n; i++) {
         cin >> a[i];
         a[i + n] = a[i];
     }
-    for (int i = 1; i <= 2 * n; i++) {
-        s[i] = s[i - 1] + a[i];
-        g[i][i] = 0;
-        f[i][i] = 0;
-    }
-    for (int len = 2; len <= n; len++) {
+    for (int len = 3; len <= n + 1; len++) {
         for (int l = 1; l + len - 1 <= 2 * n; l++) {
-            int r = l + len - 1;
-            for (int k = l; k < r; k++) {
-                f[l][r] = min(f[l][r], f[l][k] + f[k + 1][r] + s[r] - s[l - 1]);
-                g[l][r] = max(g[l][r], g[l][k] + g[k + 1][r] + s[r] - s[l - 1]);
+            int r = len + l - 1;
+            for (int k = l + 1; k < r; k++) {
+                f[l][r] = max(f[l][r], f[l][k] + f[k][r] + a[l] * a[k] * a[r]);
             }
         }
     }
-    int minv =INT_MAX, maxv = -minv;
+    int res = 0;
     for (int i = 1; i <= n; i++) {
-        minv = min(minv, f[i][i + n - 1]);
-        maxv = max(maxv, g[i][i + n - 1]);
+        res = max(res, f[i][i + n]);
     }
-    cout << minv <<endl<< maxv <<endl;
+    cout << res <<endl;
 }
