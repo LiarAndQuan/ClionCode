d74b53f53e0d8424718ebaca7c012487db6abdd2
LiarAndQuan <3218684870@qq.com>
2022-11-06 13:55:13 +0800

    数字逻辑

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,29 +5,53 @@
 #define endl '\n'
 using namespace std;
 
-int n;
-int a[200], f[200][200];
+
+inta[12], f[12][12];
+
+void init() {
+    for (int i = 0; i <= 9; i++) {
+        f[1][i] = 1;
+    }
+    for (int i = 2; i < 12; i++) {
+        for (int j = 0; j <= 9; j++) {
+            for (int k = j; k <= 9; k++) {
+                f[i][j] += f[i - 1][k];
+            }
+        }
+    }
+}
+
+int dp(int n) {
+    if (!n) {
+        return 1;
+    }
+    int cnt = 0;
+    while (n) {
+        a[++cnt] = n % 10;
+        n /= 10;
+    }
+    int res = 0, last = 0;
+    for (int i = cnt; i >= 1; i--) {
+        int now = a[i];
+        for (int j = last; j < now; j++) {
+            res += f[i][j];
+        }
+        if (now < last) {
+            break;
+        }
+        last = now;
+        if (i == 1) {
+            res++;
+        }
+    }
+    return res;
+}
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    cin >> n;
-    for (int i = 1; i <= n; i++) {
-        cin >> a[i];
-        a[i + n] = a[i];
-    }
-    for (int len = 3; len <= n + 1; len++) {
-        for (int l = 1; l + len - 1 <= 2 * n; l++) {
-            int r = len + l - 1;
-            for (int k = l + 1; k < r; k++) {
-                f[l][r] = max(f[l][r], f[l][k] + f[k][r] + a[l] * a[k] * a[r]);
-            }
-        }
-    }
-    int res = 0;
-    for (int i = 1; i <= n; i++) {
-        res = max(res, f[i][i + n]);
-    }
-    cout << res <<endl;
+    init();
+    int l, r;
+    cin >> l >> r;
+    cout << dp(r) - dp(l - 1);
 }
