444a4ec4aa68aa63540d0fd1149cea7696e48785
LiarAndQuan <3218684870@qq.com>
2022-11-07 15:28:07 +0800

    Windy数(数位DP)

--- algorithm.cpp
+++ algorithm.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 
-inta[12], f[12][12];
+int a[12], f[12][10];
 
 void init() {
     for (int i = 0; i <= 9; i++) {
@@ -14,34 +14,43 @@ void init() {
     }
     for (int i = 2; i < 12; i++) {
         for (int j = 0; j <= 9; j++) {
-            for (int k = j; k <= 9; k++) {
-                f[i][j] += f[i - 1][k];
+            for (int k = 0; k <= 9; k++) {
+                if (abs(k - j) >= 2) {
+                    f[i][j] += f[i - 1][k];
+                }
             }
         }
     }
 }
 
-int dp(int n) {
-    if (!n) {
-        return 1;
+int dp(int x) {
+    if (!x) {
+        return 0;
     }
     int cnt = 0;
-    while (n) {
-        a[++cnt] = n % 10;
-        n /= 10;
+    while (x) {
+        a[++cnt] = x % 10;
+        x /= 10;
     }
-    int res = 0, last = 0;
-    for (int i = cnt; i >= 1; i--) {
+    int res = 0, last = -2;
+    for (inti = cnt; i >= 1; i--) {
         int now = a[i];
-        for (int j = last; j < now; j++) {
-            res += f[i][j];
+        for (int j = (i == cnt); j < now; j++) {
+            if (abs(j - last) >= 2) {
+                res += f[i][j];
+            }
         }
-        if (now < last) {
+        if (abs(now - last) < 2) {
             break;
         }
         last = now;
         if (i == 1) {
             res++;
+        }
+    }
+    for (int i = 1; i < cnt; i++) {
+        for (int j = 1; j <= 9; j++) {
+            res += f[i][j];
         }
     }
     return res;
@@ -50,8 +59,10 @@ int dp(int n) {
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
     init();
     int l, r;
     cin >> l >> r;
-    cout << dp(r) - dp(l - 1);
+    cout << dp(r) - dp(l - 1) <<endl;
+
 }
