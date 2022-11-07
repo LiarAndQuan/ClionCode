0288c8dff09f5bcb6b1d3cddd3cf0c45e364e3d9
LiarAndQuan <3218684870@qq.com>
2022-11-07 15:28:33 +0800

    度的数量(数位DP)

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,56 +5,53 @@
 #define endl '\n'
 using namespace std;
 
-
-int a[12], f[12][10];
+int a[34], f[34][34], k, b;
+//f[i][j]表示在i个位置上放j个1的组合数.
 
 void init() {
-    for (int i = 0; i <= 9; i++) {
-        f[1][i] = 1;
+    for (int i = 0; i < 34; i++) {
+        f[i][0] = 1;
     }
-    for (int i = 2; i < 12; i++) {
-        for (int j = 0; j <= 9; j++) {
-            for (int k = 0; k <= 9; k++) {
-                if (abs(k - j) >= 2) {
-                    f[i][j] += f[i - 1][k];
-                }
-            }
+    for (int i = 1; i < 34; i++) {
+        for (int j = 1; j <= i; j++) {
+            f[i][j] = f[i - 1][j - 1] + f[i - 1][j];
         }
     }
 }
 
-int dp(int x) {
-    if (!x) {
+int dp(int n) {
+    if (!n) {
         return 0;
     }
     int cnt = 0;
-    while (x) {
-        a[++cnt] = x % 10;
-        x /= 10;
+    while (n) {
+        a[++cnt] = n % b;
+        n /= b;
     }
-    int res = 0, last = -2;
-    for (inti = cnt; i >= 1; i--) {
-        int now = a[i];
-        for (int j = (i == cnt); j < now; j++) {
-            if (abs(j - last) >= 2) {
-                res += f[i][j];
+    int res = 0, last = 0;
+    for (int i = cnt; i >= 1; i--) {
+        int x = a[i];
+        if (x) {
+            res += f[i - 1][k - last];
+            if (x > 1) {
+                if (k - last - 1 >= 0) {
+                    res += f[i - 1][k - last - 1];
+                }
+                break;
+            } else {
+                last++;
+                if (last > k) {
+                    break;
+                }
             }
         }
-        if (abs(now - last) < 2) {
-            break;
-        }
-        last = now;
-        if (i == 1) {
+        if (i == 1 && last == k) {
             res++;
-        }
-    }
-    for (int i = 1; i < cnt; i++) {
-        for (int j = 1; j <= 9; j++) {
-            res += f[i][j];
         }
     }
     return res;
 }
+
 
 signed main() {
     ios::sync_with_stdio(false);
@@ -62,7 +59,6 @@ signed main() {
 
     init();
     int l, r;
-    cin >> l >> r;
-    cout << dp(r) - dp(l - 1) <<endl;
-
+    cin >> l >> r >> k >> b;
+    cout << dp(r) - dp(l - 1);
 }
