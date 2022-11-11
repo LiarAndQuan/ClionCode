b894b8e32474e5b57c39db4c790aed8a48b80eb3
LiarAndQuan <3218684870@qq.com>
2022-11-11 11:00:41 +0800

    字典树

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,44 +1,30 @@
-#include<bits/stdc++.h>
+#include<iostream>
 
 using namespace std;
 
+int ch[100][26];
+int cnt[100], idx;
 
-char ori[11000005];
-char s[22000005];
-int d[22000005];
-
-int main() {
-    scanf("%s", ori);
-    int n = strlen(ori);
-
-//扩展数组
-    int k = 0;
-    s[k++] = '#';
-    for (int i = 0; i < n; i++) {
-        s[k++] = ori[i];
-        s[k++] = '#';
-    }
-
-    n = k;
-    memset(d, 0, sizeof(d));
-    d[0] = 1;
-    int l, r = 0;
-    for (int i = 1; i < n; i++) {
-        if (i <= r) {
-            d[i] = min(d[r - i + l], r - i + 1);
+void insert(char a[]) {
+    int p = 0;
+    for (int i = 0; a[i]; i++) {
+        int j = a[i] - 'a';
+        if (!ch[p][j]) {
+            ch[p][j] = ++idx;
         }
-        while (i - d[i] != -1 && s[i - d[i]] == s[i + d[i]]) {//加上前置条件,以防数组越界
-            d[i]++;
-        }
-        if (i + d[i] - 1 > r) {
-            l = i - d[i] + 1;
-            r = i + d[i] - 1;
-        }
+        p = ch[p][j];
     }
+    cnt[p]++;
+}
 
-    int mx = -1;
-    for (int i = 0; i < n; i++) {
-        mx = max(mx, d[i]);
+int query(char a[]) {
+    int p = 0;
+    for (int i = 0; a[i]; i++) {
+        int j = a[i] - 'a';
+        if (!ch[p][j]) {
+            return 0;
+        }
+        p = ch[p][j];
     }
-    cout << mx - 1;
+    return cnt[p];
 }
