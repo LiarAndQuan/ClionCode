159d91500df1ff06b21a6e9f83dd44a6979f66f4
LiarAndQuan <3218684870@qq.com>
2022-11-11 11:01:09 +0800

    最大异或对

--- algorithm.cpp
+++ algorithm.cpp
@@ -2,29 +2,46 @@
 
 using namespace std;
 
-int ch[100][26];
-int cnt[100], idx;
+int ch[1000][2], idx;
 
-void insert(char a[]) {
+void insert(int n) {
     int p = 0;
-    for (int i = 0; a[i]; i++) {
-        int j = a[i] - 'a';
+    for (int i = 30; i >= 0; i--) {
+        int j = 1 & (n >> i);
         if (!ch[p][j]) {
             ch[p][j] = ++idx;
         }
         p = ch[p][j];
     }
-    cnt[p]++;
 }
 
-int query(char a[]) {
+int query(int n) {
     int p = 0;
-    for (int i = 0; a[i]; i++) {
-        int j = a[i] - 'a';
-        if (!ch[p][j]) {
-            return 0;
+    int res = 0;
+    for (int i = 30; i >= 0; i--) {
+        int j = 1 & (n >> i);
+        if (ch[p][!j]) {
+            res += 1 << i;
+            p = ch[p][!j];
+        } else {
+            p = ch[p][j];
         }
-        p = ch[p][j];
     }
-    return cnt[p];
+    return res;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int a[100];
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        insert(a[i]);
+    }
+    int ma = -1;
+    for (int i = 1; i <= n; i++) {
+        ma = max(ma, query(a[i]));
+    }
+    cout << ma;
+
 }
