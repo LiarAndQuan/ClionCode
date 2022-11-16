bc382b2c234097682d6fca49f6ab26a01a84ecc9
LiarAndQuan <3218684870@qq.com>
2022-11-16 19:50:47 +0800

    并查集

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,25 +1,43 @@
 #include<bits/stdc++.h>
-using namespace std;
-string inorder, postorder;
-void f(int inl, int inr, int postl, int postr) {
-    if (inl > inr) {
-        cout << "#";
-        return;
-    }
-    cout << postorder[postr];
-    int index;
 
-    for (int i = inl; i <= inr; i++) {
-        if (inorder[i] == postorder[postr]) {
-            index = i;
+#define int long long
+#define double long double
+#define endl '\n'
+using namespace std;
+
+int fa[10005];
+
+int find(int i) {
+    if (fa[i] == i) {
+        return i;
+    } else {
+        return fa[i] = find(fa[i]);
+    }
+}
+
+void unionset(int x, int y) {
+    fa[find(x)] = find(y);
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < 10000; i++) {
+        fa[i] = i;
+    }
+    for (int i = 1; i <= m; i++) {
+        int x, y, z;
+        cin >> z >> x >> y;
+        if (z == 1) {
+            unionset(x, y);
+        } else {
+            if (find(x) == find(y)) {
+                cout << "Y" << endl;
+            } else {
+                cout << "N" << endl;
+            }
         }
     }
-    index = index - inl;//长度
-    f(inl, inl + index - 1, postl, postl + index - 1);
-    f(inl + index + 1, postr - postl + inl, postl + index, postr - 1);
-
-}
-int main() {
-    cin >> postorder >> inorder;
-    f(0, inorder.length() - 1, 0, postorder.length() - 1);
 }
