d12d79a860a41cfdbe47bbd3cd04f00acd98f950
LiarAndQuan <3218684870@qq.com>
2022-11-21 21:07:38 +0800

    跳马问题(洛谷P1644)

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,29 +5,21 @@
 #define endl '\n'
 using namespace std;
 
-int n, m;
-int a, b, x, y;
-int arr[200][200];
-int res;
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, 1, 0, -1};
-int vis[200][200];
+int n, m, ans;
+int dx[4] = {2, 1, -1, -2};
+int dy[4] = {1, 2, 2, 1};
 
 
 void dfs(int nowx, int nowy) {
-
-    if (nowx == x && nowy == y) {
-        res++;
+    if (nowx == n && nowy == m) {
+        ans++;
         return;
     }
-
     for (int i = 0; i < 4; i++) {
         int xx = nowx + dx[i];
         int yy = nowy + dy[i];
-        if (xx >= 1 && xx <= n && yy >= 1 && yy <= m && vis[xx][yy] == 0 && arr[xx][yy] == 0) {
-            vis[xx][yy] = 1;
+        if (xx >= 0 && xx <= n && yy >= 0 && yy <= m ) {
             dfs(xx, yy);
-            vis[xx][yy] = 0;
         }
     }
 
@@ -37,15 +29,8 @@ signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t;
-    cin >> n >> m >> t;
-    cin >> a >> b >> x >> y;
-    for (int i = 1; i <= t; i++) {
-        int w, v;
-        cin >> w >> v;
-        arr[w][v] = 1;
-    }
-    vis[a][b] = 1;
-    dfs(a, b);
-    cout << res << endl;
+    cin >> n >> m;
+    dfs(0, 0);
+    cout <<ans<< endl;
+
 }
