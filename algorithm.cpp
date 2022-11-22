68195d1b55e39eb54f5d4c197fb5726e9c8ae2e4
LiarAndQuan <3218684870@qq.com>
2022-11-22 15:30:47 +0800

    迷宫最短路径

--- algorithm.cpp
+++ algorithm.cpp
@@ -7,48 +7,58 @@ using namespace std;
 
 
 int n, m;
-char arr[105][105];
-int ans;
+int arr[200][200];
+int a, b, x, y;
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+pair<int, int> ps[200][200];
 
-int dx[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
-int dy[8] = {-1, 0, 1, 1, 1, 0, -1, -1};
+
+void print(int i, int j) {
+    if (i == a && j == b) {
+        return;
+    } else {
+        print(ps[i][j].first, ps[i][j].second);
+        cout << ps[i][j].first << " " << ps[i][j].second << endl;
+    }
+}
 
 void bfs(int i, int j) {
     queue<pair<int, int>> q;
     q.push({i, j});
     while (q.size()) {
-        pair<int, int> p = q.front();
+        pair<int, int> cur = q.front();
         q.pop();
-        for (int k = 0; k < 8; k++) {
-            int xx = p.first + dx[k];
-            int yy = p.second + dy[k];
-            if (xx >= 1 && xx <= n && yy >= 1 && yy <= m && arr[xx][yy] == 'W') {
-                arr[xx][yy] = '.';
+        if (cur.first == x && cur.second == y) {
+            print(x, y);
+            cout << x << " " << y << endl;
+            return;
+        }
+        for (int k = 0; k < 4; k++) {
+            int xx = cur.first + dx[k];
+            int yy = cur.second + dy[k];
+            if (xx >= 1 && xx <= n && yy >= 1 && yy <= m && arr[xx][yy] == 0) {
+                arr[xx][yy] = 1;
                 q.push({xx, yy});
+                ps[xx][yy] = cur;
             }
         }
     }
 }
+
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     cin >> n >> m;
+    cin >> a >> b >> x >> y;
+
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
             cin >> arr[i][j];
         }
     }
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            if (arr[i][j] == 'W') {
-                ans++;
-                arr[i][j] = '.';
-                bfs(i, j);
-            }
-        }
-    }
-    cout << ans << endl;
-
+    bfs(a, b);
 }
+
