c874dc4bf1283ef68cae15c9401f23fff98274e8
LiarAndQuan <3218684870@qq.com>
2022-11-23 17:21:52 +0800

    八数码问题(洛谷P1379)

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,54 +5,43 @@
 #define endl '\n'
 using namespace std;
 
-
-int g[100][100];
-int dis[100][100];
+unordered_map<string,int> d;
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
-int n, m;
 
-
-void bfs() {
-    memset(dis, -1, sizeof(dis));
-    queue<pair<int, int>> q;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            if (g[i][j] == 1) {
-                q.push({i, j});
-                dis[i][j] = 0;
-            }
-        }
-    }
-
+int bfs(string str, string end) {
+    queue<string> q;
+    q.push(str);
     while (q.size()) {
-        auto cur = q.front();
+        auto s = q.front();
         q.pop();
+        if (s == end) {
+            return d[s];
+        }
+        int k = s.find('0');
+        int x = k / 3;
+        int y = k % 3;
         for (int i = 0; i < 4; i++) {
-            int xx = cur.first + dx[i];
-            int yy = cur.second + dy[i];
-            if (xx >= 1 && xx <= n && yy >= 1 && yy <= m && dis[xx][yy] == -1) {
-                dis[xx][yy] = dis[cur.first][cur.second] + 1;
-                q.push({xx, yy});
+            int xx = x + dx[i];
+            int yy = y + dy[i];
+            if (xx >= 0 && xx <= 2 && yy >= 0 && yy <= 2) {
+                int dis = d[s];
+                swap(s[k], s[xx * 3 + yy]);
+                if (!d.count(s)) {
+                    d[s] = dis + 1;
+                    q.push(s);
+                }
+                swap(s[k], s[xx * 3 + yy]);
             }
         }
     }
+    return 0;
 }
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            cin >> g[i][j];
-        }
-    }
-    bfs();
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            cout << dis[i][j] << " ";
-        }
-        cout << endl;
-    }
+    string str;
+    cin >> str;
+    cout << bfs(str, "123804765");
 }
