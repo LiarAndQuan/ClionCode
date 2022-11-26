7fa6894fbab25eb02709994977dc7b8d51d1bace
LiarAndQuan <3218684870@qq.com>
2022-11-26 10:12:03 +0800

    最小环

--- algorithm.cpp
+++ algorithm.cpp
@@ -2,112 +2,48 @@
 
 #define int long long
 #define double long double
-#define endl'\n'
+#define endl '\n'
 using namespace std;
 
-struct edge {
-    int v, w;
-};
 
-vector<edge> e[3005];
-int vis[3005];
 int n, m;
-int d[3005], times[3005], h[3005];
-
-bool spfa(int s) {
-    queue<int> q;
-    memset(vis, 0, sizeof vis);
-    for (inti = 0; i <= n; i++) {
-        h[i] = 1e9;
-    }
-    h[s] = 0;
-    vis[s] = 1;
-    q.push(s);
-    while (q.size()) {
-        int u = q.front();
-        q.pop();
-        vis[u] = false;
-        for (edge ed: e[u]) {
-            int v = ed.v;
-            int w = ed.w;
-            if (h[v] > h[u] + w) {
-                h[v] = h[u] + w;
-                times[v] = times[u] + 1;
-                if (times[v] > n) {//建立虚拟源点之后一共有n+1个点
-                    return true;
-                }
-                if (!vis[v]) {
-                    vis[v] = true;
-                    q.push(v);
-                }
-            }
-        }
-    }
-    return false;
-}
-
-void dijkstra(ints) {
-    priority_queue<pair<int,int>> q;
-    memset(vis, 0, sizeof vis);
-    for (inti = 0; i <= n; i++) {
-        d[i] = 1e9;
-    }
-    d[s] = 0;
-    q.push({0, s});
-    while (q.size()) {
-        auto t = q.top();
-        q.pop();
-        if (vis[t.second]) {
-            continue;
-        }
-        vis[t.second] = 1;
-        for (auto ed: e[t.second]) {
-            int v = ed.v;
-            int w = ed.w;
-            if (d[v] > d[t.second] + w) {
-                d[v] = d[t.second] + w;
-                q.push({-d[v], v});
-            }
-        }
-    }
-}
+int w[105][105];
+int d[105][105];
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     cin >> n >> m;
-    for (inti = 1; i <= m; i++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        e[a].push_back({b, c});
-    }
-
     for (int i = 1; i <= n; i++) {
-        e[0].push_back({i, 0});
-    }
-
-    if (spfa(0)) {
-        cout << "-1 " <<endl;
-        return 0;
-    }
-
-    for (int i = 1; i <= n; i++) {
-        for (auto &ed: e[i]) {
-            ed.w += h[i] - h[ed.v];
-        }
-    }
-
-    for (int i = 1; i <= n; i++) {
-        int ans = 0;
-        dijkstra(i);
         for (int j = 1; j <= n; j++) {
-            if (d[j] == pow(10, 9)) {
-                ans += j * pow(10, 9);
-            } else {
-                ans += j * (d[j] + h[j] - h[i]);
+            if (i != j) {
+                w[i][j] = 0x7f7f7f7f;
             }
         }
+    }
+    for (int i = 1; i <= m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        w[a][b] = w[b][a] = c;
+    }
+    int ans = 0x7f7f7f7f;
+    memcpy(d, w, sizeof d);
+    for (int k = 1; k <= n; k++) {//在尚未开始第K轮循环时,假设最小环中编号最大的顶点为k
+        for (int i = 1; i < k; i++) {//枚举相邻的两个点
+            for (int j = i + 1; j < k; j++) {
+                ans = min(ans, d[i][j] + w[j][k] + w[k][i]);
+            }
+        }
+        for (int i = 1; i <= n; i++) {//floyd算法
+            for (int j = 1; j <= n; j++) {
+                d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+            }
+        }
+    }
+    if (ans == 0x7f7f7f7f) {
+        cout << "No solution." <<endl;
+    } else {
         cout << ans <<endl;
     }
 }
