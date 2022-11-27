c3aeda98e674f3a6c1a9faf3f411fb9ae3f7bf57
LiarAndQuan <3218684870@qq.com>
2022-11-27 10:30:27 +0800

    最近公共祖先(Tarjan)

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,65 +5,60 @@
 #define endl '\n'
 using namespace std;
 
-
-const int N = 5e5 + 10;
-int n, m, s;
+#define N 600000
+#define M 600000
 vector<int> e[N];
-int dep[N], fa[N][35];
-int head[N];
+vector<pair<int,int>> query[N];
+int fa[N], vis[N], ans[M];
 
 
-void dfs(int u,int father) {
-    dep[u] = dep[father] + 1;
-    fa[u][0] = father;
-    for (int i = 1; i <= 31; i++) {
-        fa[u][i] = fa[fa[u][i - 1]][i - 1];
-    }
-    for (int v: e[u]) {
-        if (v != father) {
-            dfs(v, u);
-        }
-    }
-
-}
-
-int lca(int u,int v) {
-    if (dep[u] < dep[v]) {//确保u为深度更大的点
-        swap(u, v);
-    }
-    for (int i = 31; i >= 0; i--) {//先跳到同一层
-        if (dep[fa[u][i]] >= dep[v]) {//如果u跳完,v还在u的上面,那就可以跳
-            u = fa[u][i];
-        }
-    }
-    if (u == v) {
+int find(int u) {
+    if (fa[u] == u) {
         return u;
+    } else {
+        return fa[u] = find(fa[u]);
     }
-    for (int i = 31; i >= 0; i--) {
-        if (fa[u][i] != fa[v][i]) {//跳完之后不相等的话,那就可以跳
-            u = fa[u][i];
-            v = fa[v][i];
-        }
-    }
-    return fa[u][0];
 }
 
+void tarjan(int u) {
+    vis[u] = true;
+    for (auto v: e[u]) {
+        if (!vis[v]) {
+            tarjan(v);
+            fa[v] = u;
+        }
+    }
+    for (auto q: query[u]) {
+        int v = q.first;
+        int i = q.second;
+        if (vis[v]) {
+            ans[i] = find(v);
+        }
+    }
+}
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    int n, m, s;
     cin >> n >> m >> s;
-    for (int i = 1; i <= n - 1; i++) {
+    for (int i = 1; i < n; i++) {
         int a, b;
         cin >> a >> b;
         e[a].push_back(b);
         e[b].push_back(a);
     }
-    dfs(s, 0);
     for (int i = 1; i <= m; i++) {
-        int x, y;
-        cin >> x >> y;
-        cout << lca(x, y) <<endl;
+        int a, b;
+        cin >> a >> b;
+        query[a].push_back({b, i});
+        query[b].push_back({a, i});
+    }
+    for (int i = 1; i <= n; i++) {
+        fa[i] = i;
+    }
+    tarjan(s);
+    for (int i = 1; i <= m; i++) {
+        cout << ans[i] <<endl;
     }
 }
