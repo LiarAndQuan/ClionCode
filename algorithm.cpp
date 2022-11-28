bd7f9658779fe54de95a01aa7a79704f70d8e060
LiarAndQuan <3218684870@qq.com>
2022-11-28 17:23:22 +0800

    最近公共祖先(树链剖分)

--- algorithm.cpp
+++ algorithm.cpp
@@ -7,58 +7,68 @@ using namespace std;
 
 #define N 600000
 #define M 600000
+
 vector<int> e[N];
-vector<pair<int,int>> query[N];
-int fa[N], vis[N], ans[M];
+int fa[N], dep[N], son[N], sz[N];
+int top[N];
+int n, m, s;
 
-
-int find(int u) {
-    if (fa[u] == u) {
-        return u;
-    } else {
-        return fa[u] = find(fa[u]);
+void dfs1(int u,int father) {
+    fa[u] = father;
+    dep[u] = dep[father] + 1;
+    sz[u] = 1;
+    for (int v: e[u]) {
+        if (v == father) {
+            continue;
+        }
+        dfs1(v, u);
+        sz[u] += sz[v];
+        if (sz[son[u]] < sz[v]) {
+            son[u] = v;
+        }
     }
 }
 
-void tarjan(int u) {
-    vis[u] = true;
-    for (auto v: e[u]) {
-        if (!vis[v]) {
-            tarjan(v);
-            fa[v] = u;
-        }
+void dfs2(int u,int t) {
+    top[u] = t;
+    if (!son[u]) {
+        return;
     }
-    for (auto q: query[u]) {
-        int v = q.first;
-        int i = q.second;
-        if (vis[v]) {
-            ans[i] = find(v);
+    dfs2(son[u], t);
+    for (int v: e[u]) {
+        if (v == fa[u] || v == son[u]) {
+            continue;
         }
+        dfs2(v, v);
     }
+}
+
+int lca(int u,int v) {
+    while (top[u] != top[v]) {//不在一条链上
+        if (dep[top[u]] < dep[top[v]]) {//保证始终是u往上跳
+            swap(u, v);
+        }
+        u = fa[top[u]];
+    }
+    return dep[u] < dep[v] ? u : v;//在同一条重链上,深度较浅的返回
 }
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n, m, s;
     cin >> n >> m >> s;
+
     for (int i = 1; i < n; i++) {
         int a, b;
         cin >> a >> b;
         e[a].push_back(b);
         e[b].push_back(a);
     }
+    dfs1(s, 0);
+    dfs2(s, s);
     for (int i = 1; i <= m; i++) {
         int a, b;
         cin >> a >> b;
-        query[a].push_back({b, i});
-        query[b].push_back({a, i});
-    }
-    for (int i = 1; i <= n; i++) {
-        fa[i] = i;
-    }
-    tarjan(s);
-    for (int i = 1; i <= m; i++) {
-        cout << ans[i] <<endl;
+        cout << lca(a, b) <<endl;
     }
 }
