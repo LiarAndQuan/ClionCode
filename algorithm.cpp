e3a90b3192d840af8fac5f0b8349112c2854a9a3
LiarAndQuan <3218684870@qq.com>
2022-11-29 15:49:22 +0800

    洛谷P3387权值和最大

--- algorithm.cpp
+++ algorithm.cpp
@@ -2,17 +2,21 @@
 
 #define int long long
 #define double long double
-#define endl '\n'
+#define endl'\n'
 using namespace std;
 
 
 #define N 100000
 
 vector<int> e[N];
+vector<int> ne[N];
 int dfn[N], low[N], tot;
 int scc[N], cnt;
 stack<int> s;
 int instack[N];
+int w[N];
+int wscc[N];
+int dp[N];
 
 void tarjan(int u) {
     dfn[u] = low[u] = ++tot;
@@ -34,45 +38,52 @@ void tarjan(int u) {
             s.pop();
             instack[v] = 0;
             scc[v] = cnt;
+            wscc[cnt] += w[v];
         }
     }
 }
 
 signed main() {
+
+
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int n, m;
     cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        cin >> w[i];
+    }
     for (int i = 1; i <= m; i++) {
-        int a, b;
-        cin >> a >> b;
-        e[a].push_back(b);
+        int u, v;
+        cin >> u >> v;
+        e[u].push_back(v);
     }
     for (int i = 1; i <= n; i++) {
         if (!dfn[i]) {
             tarjan(i);
         }
     }
-    int out[N];
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++) {//建立拓扑图
         for (int j: e[i]) {
-            if (scc[i] != scc[j]) {
-                out[scc[i]]++;
+            int a = scc[i];
+            int b = scc[j];
+            if (a != b) {
+                ne[a].push_back(b);
             }
         }
     }
-    int ans = 0;
-    set<int> se;
-    for (int i = 1; i <= n; i++) {
-        if (out[scc[i]] == 0) {
-            se.insert(scc[i]);
-            ans++;
+    for (int i = cnt; i >= 0; i--) {//根据拓扑图序逆推
+        if (dp[i] == 0) {
+            dp[i] = wscc[i];
+        }
+        for (int j: ne[i]) {
+            dp[j] = max(dp[j], dp[i] + wscc[j]);
         }
     }
-    if (se.size() > 1) {
-        cout << 0 <<endl;
-    } else {
-        cout << ans <<endl;
+    int ma = -1;
+    for (int i = 1; i <= cnt; i++) {
+        ma = max(ma, dp[i]);
     }
+    cout << ma <<endl;
 }
