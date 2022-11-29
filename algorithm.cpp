544afe2a82aacf7dc6b4ef9eb8900e27c4889c80
LiarAndQuan <3218684870@qq.com>
2022-11-29 15:48:57 +0800

    洛谷P2341受欢迎的牛(scc)

--- algorithm.cpp
+++ algorithm.cpp
@@ -6,13 +6,13 @@
 using namespace std;
 
 
-#define N 200000
+#define N 100000
+
 vector<int> e[N];
 int dfn[N], low[N], tot;
-int instack[N];
-stack<int> s;
 int scc[N], cnt;
-int in[N], out[N];
+stack<int> s;
+int instack[N];
 
 void tarjan(int u) {
     dfn[u] = low[u] = ++tot;
@@ -27,8 +27,8 @@ void tarjan(int u) {
         }
     }
     if (dfn[u] == low[u]) {
-        cnt++;
         int v = -1;
+        cnt++;
         while (v != u) {
             v = s.top();
             s.pop();
@@ -42,44 +42,37 @@ signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n;
-    cin >> n;
-    for (int i = 1; i <= n; i++) {
-        int v;
-        while (1) {
-            cin >> v;
-            if (v == 0) {
-                break;
-            }
-            e[i].push_back(v);
-        }
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= m; i++) {
+        int a, b;
+        cin >> a >> b;
+        e[a].push_back(b);
     }
     for (int i = 1; i <= n; i++) {
         if (!dfn[i]) {
             tarjan(i);
         }
     }
+    int out[N];
     for (int i = 1; i <= n; i++) {
         for (int j: e[i]) {
             if (scc[i] != scc[j]) {
-                in[scc[j]]++;
                 out[scc[i]]++;
             }
         }
     }
-    int a = 0, b = 0;
-    for (int i = 1; i <= cnt; i++) {
-        if (!in[i]) {
-            a++;
-        }
-        if (!out[i]) {
-            b++;
+    int ans = 0;
+    set<int> se;
+    for (int i = 1; i <= n; i++) {
+        if (out[scc[i]] == 0) {
+            se.insert(scc[i]);
+            ans++;
         }
     }
-    cout << a <<endl;//入度为0的点就是需要建立的点
-    if (cnt == 1) {//如果只有一个缩点,那么需要特判,否则入度与出度为0的数量都会变为1,输出1,错误
+    if (se.size() > 1) {
         cout << 0 <<endl;
     } else {
-        cout << max(a, b) <<endl;
+        cout << ans <<endl;
     }
 }
