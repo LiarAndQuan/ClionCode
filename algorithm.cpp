0fd92e5b2d954336192897bb033e969ca1465454
LiarAndQuan <3218684870@qq.com>
2022-12-01 21:05:12 +0800

    割边

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,60 +5,69 @@
 #define endl '\n'
 using namespace std;
 
-#define N 100000
-vector<int> e[N];
-int dfn[N], low[N], tot;
-int root;
-int cut[N];
+#define N 10000
 
+struct edge {
+    int u;
+    int v;
+};
 
-void tarjan(int x) {
+vector<edge> e;
+vector<int> h[N];
+int dfn[N], low[N], tot, cnt;
+
+struct bridge {
+    int x;
+    int y;
+    bool operator<(const bridge &b) const {
+        if (x == b.x) {
+            return y < b.y;
+        }
+        return x < b.x;
+    }
+} bridge[2 *N];
+
+void add(int a,int b) {
+    e.push_back({a, b});
+    h[a].push_back(e.size() - 1);
+}
+
+void tarjan(int x,int in_edge) {
     dfn[x] = low[x] = ++tot;
-    int child = 0;
-    for (int y: e[x]) {
-        if (!dfn[y]) {
-            tarjan(y);
+    for (int i = 0; i < h[x].size(); i++) {
+        int j = h[x][i];
+        int y = e[j].v;
+        if (!dfn[y]) {//如果y没有访问过
+            tarjan(y, j);
             low[x] = min(low[x], low[y]);
-            if (low[y] >= dfn[x]) {
-                child++;
-                if (x != root || child > 1) {
-                    cut[x] = true;
-                }
+            if (low[y] > dfn[x]) {//满足割边条件
+                bridge[++cnt] = {x, y};
             }
-        } else {
+        } else if (j != (in_edge ^ 1)) {//如果访问过并且不是走的反边
             low[x] = min(low[x], dfn[y]);
         }
     }
 }
 
-
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
     int n, m;
     cin >> n >> m;
     for (int i = 1; i <= m; i++) {
         int a, b;
         cin >> a >> b;
-        e[a].push_back(b);
-        e[b].push_back(a);
+        add(a, b);
+        add(b, a);
     }
     for (int i = 1; i <= n; i++) {
         if (!dfn[i]) {
-            root = i;
-            tarjan(i);
+            tarjan(i, -2);
         }
     }
-    int sum = 0;
-    for (int i = 1; i <= n; i++) {
-        if (cut[i]) {
-            sum++;
-        }
-    }
-    cout << sum <<endl;
-    for (int i = 1; i <= n; i++) {
-        if (cut[i]) {
-            cout << i << " ";
-        }
+    sort(bridge + 1, bridge + 1 + cnt);
+    for (inti = 1; i <= cnt; i++) {
+        cout << bridge[i].x << " " << bridge[i].y <<endl;
     }
 }
