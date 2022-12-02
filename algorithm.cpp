4ab8667f8e42edbb40ddcfac4eadc50f52629f14
LiarAndQuan <3218684870@qq.com>
2022-12-02 21:54:26 +0800

    边双连通分量

--- algorithm.cpp
+++ algorithm.cpp
@@ -6,45 +6,50 @@
 using namespace std;
 
 #define N 10000
+#define M 20000
 
 struct edge {
-    int u;
-    int v;
-};
+		int v;
+		int ne;
+} e[M];
 
-vector<edge> e;
-vector<int> h[N];
-int dfn[N], low[N], tot, cnt;
+int h[N], idx = 1;
+int dfn[N], low[N], tot;
+stack<int> s;
+int dcc[N], cnt;
+int bri[M], d[N];
 
-struct bridge {
-    int x;
-    int y;
-    bool operator<(const bridge &b) const {
-        if (x == b.x) {
-            return y < b.y;
-        }
-        return x < b.x;
-    }
-} bridge[2 *N];
 
 void add(int a,int b) {
-    e.push_back({a, b});
-    h[a].push_back(e.size() - 1);
+    e[++idx].v = b;
+    e[idx].ne = h[a];
+    h[a] = idx;
 }
 
 void tarjan(int x,int in_edge) {
     dfn[x] = low[x] = ++tot;
-    for (int i = 0; i < h[x].size(); i++) {
-        int j = h[x][i];
-        int y = e[j].v;
-        if (!dfn[y]) {//如果y没有访问过
-            tarjan(y, j);
+    s.push(x);
+    for (int i = h[x]; i; i = e[i].ne) {//割边
+				int y = e[i].v;
+        if (!dfn[y]) {
+            tarjan(y, i);
             low[x] = min(low[x], low[y]);
-            if (low[y] > dfn[x]) {//满足割边条件
-                bridge[++cnt] = {x, y};
+            if (low[y] > dfn[x]) {
+                bri[i] = bri[i ^ 1] = true;
             }
-        } else if (j != (in_edge ^ 1)) {//如果访问过并且不是走的反边
+        } else if (i != (in_edge ^ 1)) {
             low[x] = min(low[x], dfn[y]);
+        }
+    }
+    if (dfn[x] == low[x]) {//缩点
+				cnt++;
+        while (1) {
+						int y = s.top();
+            s.pop();
+            dcc[y] = cnt;
+            if (y == x) {
+                break;
+            }
         }
     }
 }
@@ -53,21 +58,26 @@ signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, m;
+		int n, m;
     cin >> n >> m;
     for (int i = 1; i <= m; i++) {
-        int a, b;
+				int a, b;
         cin >> a >> b;
         add(a, b);
         add(b, a);
     }
-    for (int i = 1; i <= n; i++) {
-        if (!dfn[i]) {
-            tarjan(i, -2);
+    tarjan(1, 0);
+    for (int i = 2; i <= idx; i++) {
+        if (bri[i]) {
+            d[dcc[e[i].v]]++;
         }
     }
-    sort(bridge + 1, bridge + 1 + cnt);
-    for (inti = 1; i <= cnt; i++) {
-        cout << bridge[i].x << " " << bridge[i].y <<endl;
+		int sum = 0;
+    for (int i = 1; i <= cnt; i++) {
+        if (d[i] == 1) {
+            sum++;
+        }
     }
+    cout << (sum + 1) / 2;
+
 }
