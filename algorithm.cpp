1cb7b988fe1be15dedbd7cf6dcaf3c8e37d268b5
LiarAndQuan <3218684870@qq.com>
2022-12-04 19:52:09 +0800

    洛谷P1344追查坏牛奶(最大流最小割 )

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,35 +5,38 @@
 #define endl '\n'
 using namespace std;
 
-#define M 1000006
-#define N 1000006
+
+#define N 100005
+#define M 100006
 
 struct edge {
     int v;
     int c;
     int ne;
-} e[M];
 
-int h[N], idx = 1;
+    bool operator<(const edge &e) const {
+        return c > e.c;
+    }
+} e[N];
+
+int d[N];
+
 int s, t;
-int d[N], cur[N];
+int h[N], idx = 1;
+int cur[N];
 
-void add(int a, int b, int c) {
-    e[++idx] = {b, c, h[a]};
-    h[a] = idx;
-}
 
-bool bfs() {//寻找增广路,并且按照图层划分
-    memset(d, 0, sizeof d);//每一个图层置0
+bool bfs() {
+    memset(d, 0, sizeof d);
+    d[s] = 1;
     queue<int> q;
     q.push(s);
-    d[s] = 1;//源点图层为1
     while (q.size()) {
         int u = q.front();
         q.pop();
-        for (int i = h[u]; i; i = e[i].ne) {//枚举边
+        for (int i = h[u]; i; i = e[i].ne) {
             int v = e[i].v;
-            if (d[v] == 0 && e[i].c) {//如果没访问过并且有边可以去
+            if (!d[v] && e[i].c) {
                 d[v] = d[u] + 1;
                 q.push(v);
                 if (v == t) {
@@ -50,21 +53,21 @@ int dfs(int u, int mf) {
         return mf;
     }
     int sum = 0;
-    for (int i = cur[u]; i; i = e[i].ne) {//当前弧优化,cur[u]存的是之前u点最后访问的一条边,因为已经访问过的边不需要再访问
+    for (int i = cur[u]; i; i = e[i].ne) {
         cur[u] = i;
         int v = e[i].v;
-        if (d[v] == d[u] + 1 && e[i].c) {//如果在下一层并且有路可以走
-            int f = dfs(v, min(mf, e[i].c));//对子节点dfs,mf去较小值
-            e[i].c -= f;//构建残余网
+        if (d[v] == d[u] + 1 && e[i].c) {
+            int f = dfs(v, min(mf, e[i].c));
+            e[i].c -= f;
             e[i ^ 1].c += f;
+            mf -= f;
             sum += f;
-            mf -= f;//余量减小
-            if (mf == 0) {
-                break;
-            }
+        }
+        if (mf == 0) {
+            break;
         }
     }
-    if (sum == 0) {//说明这个点不能到达t点,使其无法访问即可
+    if (sum == 0) {
         d[u] = 0;
     }
     return sum;
@@ -72,26 +75,35 @@ int dfs(int u, int mf) {
 
 int dinic() {
     int flow = 0;
-    while (bfs()) {//如果有可行流
-        memcpy(cur, h, sizeof h);//每次遍历之前都重置cur
+    while (bfs()) {
+        memcpy(cur, h, sizeof h);
         flow += dfs(s, 1e9);
     }
     return flow;
+}
+
+void add(int a, int b, int c) {
+    e[++idx] = {b, c, h[a]};
+    h[a] = idx;
 }
 
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    int u[1005];
+    int v[1005];
+    int w[1005];
     int n, m;
-    cin >> n >> m >> s >> t;
-    int u, v, w;
-    for (int i = 1; i <= m; i++) {
-        cin >> u >> v >> w;
-        add(u, v, w);
-        add(v, u, 0);
-    }
-    cout << dinic();
+    cin >> n >> m;
+    s = 1;
+    t = n;
 
+    for (int i = 1; i <= m; i++) {
+        cin >> u[i] >> v[i] >> w[i];
+        add(u[i], v[i], w[i] * 2018 + 1);
+        add(v[i], u[i], 0);
+    }
+    int a = dinic();
+    cout << a / 2018 << " " << a % 2018;
 }
