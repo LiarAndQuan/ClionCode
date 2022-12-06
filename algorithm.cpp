53db12f3fe18cc5493dc81304d32eb040633253f
LiarAndQuan <3218684870@qq.com>
2022-12-06 13:39:48 +0800

    费用流

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,105 +5,80 @@
 #define endl '\n'
 using namespace std;
 
-
-#define N 100005
-#define M 100006
+#define N 50000
 
 struct edge {
     int v;
     int c;
+    int w;
     int ne;
+} e[2000000];
 
-    bool operator<(const edge &e) const {
-        return c > e.c;
-    }
-} e[N];
-
-int d[N];
-
-int s, t;
 int h[N], idx = 1;
-int cur[N];
+int d[N], mf[N], pre[N], vis[N];
+int flow, cost;
+int s, t;
 
+void add(int a, int b, int c, int x) {
+    e[++idx] = {b, c, x, h[a]};
+    h[a] = idx;
+}
 
-bool bfs() {
-    memset(d, 0, sizeof d);
-    d[s] = 1;
+bool spfa() {
+    memset(d, 0x3f, sizeof d);
+    memset(mf, 0, sizeof mf);
     queue<int> q;
     q.push(s);
+    d[s] = 0;
+    mf[s] = 0x3f3f3f3f;
+    vis[s] = 1;
     while (q.size()) {
         int u = q.front();
         q.pop();
+        vis[u] = 0;
         for (int i = h[u]; i; i = e[i].ne) {
             int v = e[i].v;
-            if (!d[v] && e[i].c) {
-                d[v] = d[u] + 1;
-                q.push(v);
-                if (v == t) {
-                    return true;
+            int c = e[i].c;
+            int w = e[i].w;
+            if (d[v] > d[u] + w && c) {
+                d[v] = d[u] + w;
+                mf[v] = min(mf[u], c);
+                pre[v] = i;
+                if (!vis[v]) {
+                    q.push(v);
+                    vis[v] = 1;
                 }
             }
         }
     }
-    return false;
+    return mf[t] > 0;//可行流能到达t点
 }
 
-int dfs(int u, int mf) {
-    if (u == t) {
-        return mf;
-    }
-    int sum = 0;
-    for (int i = cur[u]; i; i = e[i].ne) {
-        cur[u] = i;
-        int v = e[i].v;
-        if (d[v] == d[u] + 1 && e[i].c) {
-            int f = dfs(v, min(mf, e[i].c));
-            e[i].c -= f;
-            e[i ^ 1].c += f;
-            mf -= f;
-            sum += f;
+void EK() {
+    while (spfa()) {
+        for (int v = t; v != s;) {
+            int i = pre[v];
+            e[i].c -= mf[t];
+            e[i ^ 1].c += mf[t];
+            v = e[i ^ 1].v;
         }
-        if (mf == 0) {
-            break;
-        }
+        flow += mf[t];
+        cost += mf[t] * d[t];//累加费用
     }
-    if (sum == 0) {
-        d[u] = 0;
-    }
-    return sum;
-}
-
-int dinic() {
-    int flow = 0;
-    while (bfs()) {
-        memcpy(cur, h, sizeof h);
-        flow += dfs(s, 1e9);
-    }
-    return flow;
-}
-
-void add(int a, int b, int c) {
-    e[++idx] = {b, c, h[a]};
-    h[a] = idx;
 }
 
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int u[1005];
-    int v[1005];
-    int w[1005];
     int n, m;
-    cin >> n >> m;
-    s = 1;
-    t = n;
-
+    cin >> n >> m >> s >> t;
     for (int i = 1; i <= m; i++) {
-        cin >> u[i] >> v[i] >> w[i];
-        add(u[i], v[i], w[i] * 2018 + 1);
-        add(v[i], u[i], 0);
+        int a, b, c, x;
+        cin >> a >> b >> c >> x;
+        add(a, b, c, x);
+        add(b, a, 0, -x);
     }
-    int a = dinic();
-    cout << a / 2018 << " " << a % 2018;
+    EK();
+    cout << flow << " " << cost;
 }
