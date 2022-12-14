f9c097a4efc4ea48442691710960d270350060e4
LiarAndQuan <3218684870@qq.com>
2022-12-14 20:18:06 +0800

    射击问题

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,84 +5,62 @@
 #define endl '\n'
 using namespace std;
 
-vector<int> e[10000];
-bool is[100005];
 
-struct node {
-    int notion;
-    int a[10] = {0};
-} arr[100000];
+struct enmy {
+    double x;
+    double y;
+    double r;
+} e[1000];
 
-int k;
+int is[100][100];
 
-int *dfs(int u, int par) {
-    for (int i: e[u]) {
-        if (i == par) {
-            continue;
-        } else {
-            int *p = dfs(i, u);
-            for (int j = 1; j <= k + 1; j++) {
-                arr[u].a[j] += p[j];
-            }
-            free(p);
-        }
-    }
-    int *p = (int *) malloc(4 * (10));
-    for (int i = 1; i <= k + 1; i++) {
-        p[i] = arr[u].a[i];
-    }
-    return p;
-}
 
 signed main() {
-
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n, k, r;
+    cin >> n >> k >> r;
+    for (int i = 1; i <= n; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        e[i].x = a;
+        e[i].y = b;
+        e[i].r = c;
+        is[a][b] = 1;
+    }
 
-    for (int i = 1; i <= 10000; i++) {//素数
-        for (int j = 2; j <= (int) sqrt(i) + 1; j++) {
-            if (i % j == 0) {
-                is[i] = true;
-                break;
+    int sum = 0;
+
+    while (k--) {
+        int ma = 0;
+        int pi, pj;
+        vector<int> v[20][20];
+        for (int i = 0; i <= 15; i++) {
+            for (int j = 0; j <= 15; j++) {
+                for (int x = 1; x <= n; x++) {
+                    double a = e[x].x;
+                    double b = e[x].y;
+                    double c = e[x].r;
+                    if (!is[(int) a][(int) b]) {//如果敌人不存在
+                        continue;
+                    }
+                    if ((a - i) * (a - i) + (b - j) * (b - j) <=
+                        (r + c) * (r + c)) {//如果在覆盖范围内
+                        v[i][j].push_back(x);
+                    }
+                }
+                if (v[i][j].size() >= ma) {
+                    pi = i;
+                    pj = j;
+                    ma = v[i][j].size();
+                }
             }
         }
-    }
-    is[1] = true;
-    is[2] = false;
-
-
-    int n;
-    cin >> n >> k;
-
-    int temp;
-
-    for (int i = 1; i <= n; i++) {
-        cin >> temp;
-        if (is[temp]) {//不是
-            arr[i].notion = temp % k + 2;
-            arr[i].a[temp % k + 2] = 1;
-        } else {
-            arr[i].notion = 1;
-            arr[i].a[1] = 1;
+        for (int i: v[pi][pj]) {
+            is[(int) e[i].x][(int) e[i].y] = 0;
         }
+        sum += v[pi][pj].size();
     }
-
-    for (int i = 1; i <= n - 1; i++) {
-        int u, v;
-        cin >> u >> v;
-        e[u].push_back(v);
-        e[v].push_back(u);
-    }
-
-    int q;
-    cin >> q;
-    dfs(1, 0);
-
-    for (int i = 1; i <= q; i++) {
-        int x, y;
-        cin >> x >> y;
-        cout << arr[x].notion << " " << arr[x].a[y] << endl;
-    }
-
+    cout << min(sum, n) << endl;
 }
