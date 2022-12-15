93cfd92782cf5b23365d37df1dfe840d8123ae8b
LiarAndQuan <3218684870@qq.com>
2022-12-15 16:59:38 +0800

    后缀自动机(洛谷P3804)

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,62 +5,66 @@
 #define endl '\n'
 using namespace std;
 
+#define N 2000005
 
-struct enmy {
-    double x;
-    double y;
-    double r;
-} e[1000];
+int cnt[N], ans;
+int tot = 1, np = 1;
+int fa[N], ch[N][26], len[N];
+vector<int> e[N];
 
-int is[100][100];
+void extend(int c) {
+    //p回跳指针,np新点,q链接点,nq新链接点
+    int p = np;//p指向旧点
+    np = ++tot;//np是新点
+    len[np] = len[p] + 1;//子串出现次数
+    cnt[np] = 1;
+    for (; p && !ch[p][c]; p = fa[p]) {//p沿链接边往回跳,从旧点向新点建转移边
+        ch[p][c] = np;
+    }
+    if (p == 0) {//如果c是新字符,从新点向根节点建链接边
+        fa[np] = 1;
+    } else {
+        int q = ch[p][c];//q是链接点
+        if (len[q] == len[p] + 1) {//若链接点合法,从新点向q建链接边
+            fa[np] = q;
+        } else {
+            int nq = ++tot;//nq是新链接点
+            len[nq] = len[p] + 1;//重建nq,q,np的链接边
+            fa[nq] = fa[q];
+            fa[q] = nq;
+            fa[np] = nq;
+            //指向q的转移边改为指向nq
+            for (; p && ch[p][c] == q; p = fa[p]) {
+                ch[p][c] = nq;
+            }
+            //从q发出的转移边复制给nq
+            memcpy(ch[nq], ch[q], sizeof(ch[q]));
+        }
+    }
+}
 
+void dfs(int u) {
+    for (auto v: e[u]) {
+        dfs(v);
+        cnt[u] += cnt[v];
+    }
+    if (cnt[u] > 1) {
+        ans = max(ans, cnt[u] * len[u]);
+    }
+}
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, k, r;
-    cin >> n >> k >> r;
-    for (int i = 1; i <= n; i++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        e[i].x = a;
-        e[i].y = b;
-        e[i].r = c;
-        is[a][b] = 1;
+    string str;
+    cin >> str;
+    for (int i = 0; i < str.length(); i++) {
+        extend(str[i] - 'a');
     }
-
-    int sum = 0;
-
-    while (k--) {
-        int ma = 0;
-        int pi, pj;
-        vector<int> v[20][20];
-        for (int i = 0; i <= 15; i++) {
-            for (int j = 0; j <= 15; j++) {
-                for (int x = 1; x <= n; x++) {
-                    double a = e[x].x;
-                    double b = e[x].y;
-                    double c = e[x].r;
-                    if (!is[(int) a][(int) b]) {//如果敌人不存在
-                        continue;
-                    }
-                    if ((a - i) * (a - i) + (b - j) * (b - j) <=
-                        (r + c) * (r + c)) {//如果在覆盖范围内
-                        v[i][j].push_back(x);
-                    }
-                }
-                if (v[i][j].size() >= ma) {
-                    pi = i;
-                    pj = j;
-                    ma = v[i][j].size();
-                }
-            }
-        }
-        for (int i: v[pi][pj]) {
-            is[(int) e[i].x][(int) e[i].y] = 0;
-        }
-        sum += v[pi][pj].size();
+    for (int i = 2; i <= tot; i++) {
+        e[fa[i]].push_back(i);
     }
-    cout << min(sum, n) << endl;
+    dfs(1);
+    cout << ans << endl;
 }
