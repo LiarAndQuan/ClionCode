719c8117aa58df5489e47c000f80251fcd547eef
LiarAndQuan <3218684870@qq.com>
2022-12-18 14:08:56 +0800

    程序设计期末(天梯赛的善良)

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,48 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct stu{
-    int a;
-    int b;
-    bool operator <(const stu&s )const{
-        if(a==s.a ){
-            return b>s.b;
-        }else{
-            return a>s.a;
+int main(){
+    int n;
+    cin>>n;
+    vector<int> v;
+    int ma = -0x3f3f3f3f,mi = 0x3f3f3f3f,cmi = 0,cma = 0;
+    for(int i = 1;i<=n;i++ ){
+        int x;
+        cin>>x;
+        if(x>ma ){
+            ma = x;
+            cma = 1;
+        }else if(x==ma ){
+            cma++;
+        }
+        if(x<mi ){
+            mi = x;
+            cmi = 1;
+        }else if(x==mi){
+            cmi++;
         }
     }
-}stu[10000006];
-
-int main( ){
-    int n,k,s;
-    cin>>n>>k>>s;
-    int cnt = 0;
-    for(int i = 1;i<=n;i++){
-        cin>>stu[i].a>>stu[i].b;
-    }
-    sort(stu+1,stu+n+   1);
-    for(int i =1;i<=n;i++){
-        if(stu[i].a<175){
-            cnt = i-1;
-            break;
-        }
-    }
-    int j;
-    for(int i = 1;i<=n;i+=j){
-        if(stu[i   ].a<175){
-            break;
-        }
-        int x = 0,y = 0;
-        for(j = 0;i+j<=n&&stu[i].a==stu[i+j].a;j++){
-            if(stu[i+j].b<s){
-                x++;
-            }else{
-                y++;
-            }
-        }
-        if(x>k){
-            cnt-=(x-k);
-        }
-    }
-    cout<<cnt;
+    cout<<mi<<" "<<cmi<<endl;
+    cout<<ma<<" "<<cma<<endl;
 }
